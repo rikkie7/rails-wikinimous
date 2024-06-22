7207ce3d82fc1d979e5f2d79a83d8624afb20da4
Ruiqi Guan <rickyguan1994@hotmail.com>
2024-06-22 15:58:17 +1000

    challenge complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  title = Faker::Book.title
+  content = Faker::Hacker.say_something_smart
+  article = Article.new(title: title, content: content)
+  article.save
+end
